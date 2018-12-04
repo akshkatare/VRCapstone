9295e4b814ba556096d85369c0e12224ac1f928d
Akshay Katare <akatare9@gmail.com>
2018-12-04 10:20:40 -0800

    4 dec home

---
+++ Assets/MainSceneHandInteraction.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+public class MainSceneHandInteraction : MonoBehaviour {
+
+    float tempTime;
+    bool isusing;
+    public PlayableDirector pb;
+    private void Start()
+    {
+        isusing = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enter");
+        tempTime = Time.time;    
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!isusing)
+        {
+            if (other.gameObject.layer == 13)
+            {
+                other.GetComponent<SetColors>().SetSelected();
+            }
+
+            if (Time.time - tempTime > 1f)
+            {
+                isusing = true;
+                other.GetComponent<SetColors>().SetUse();
+                pb.Play();
+                other.GetComponent<BoxCollider>().enabled = false;
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 13)
+        {
+            other.GetComponent<SetColors>().SetOriginal();
+        }
+    }
+   
+}
---
+++ Assets/SetColors.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetColors : MonoBehaviour {
+    Image img;
+    Color orgcolor;
+    private void Start()
+    {
+        img = this.GetComponent<Image>();
+        orgcolor = img.color;
+    }
+    public void SetOriginal()
+    { img.color = orgcolor; }
+    public void SetSelected()
+    { img.color = Color.blue; }
+    public void SetUse()
+    { img.color = Color.green; }
+}
